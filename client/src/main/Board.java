5b8d5072cd8bc8385f35a7db2d980ca19a667635
刘俊延 <13925588730@163.com>
2017-04-17 23:32:55 +0800

    Fix ChessBox.

--- client/src/main/Board.java
+++ client/src/main/Board.java
@@ -1,19 +1,16 @@
 package src.main;
 
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-
 /**
  * Created by touhoudoge on 2017/4/16.
  */
 public class Board {
 
-    private Stone[][] board = new Stone[19][19];
+    private Stone[][] stone = new Stone[19][19];
 
     public Board(){
         for(int i = 0; i < 19; ++i){
             for(int j = 0; j < 19; ++j){
-                board[i][j] = new Stone();
+                stone[i][j] = new Stone();
             }
         }
     }
@@ -22,23 +19,15 @@ public class Board {
 
     }
 
-    public void draw(Circle circle, int posX, int posY, int color){
-        if(color == Stone.Black){
-            circle.setFill(Color.BLACK);
-        }
-        else{
-            circle.setFill(Color.WHITE);
-        }
-        circle.setLayoutX(posX);
-        circle.setLayoutY(posY);
-        circle.setRadius(10);
+    public Stone get(int x, int y){
+        return stone[x][y];
     }
 
     public boolean check(int x, int y, int color){
-        if(board[x][y].getType() != Stone.None){
+        if(stone[x][y].getType() != Stone.None){
             return false;
         }
-        board[x][y].setType(color);
+        stone[x][y].setType(color);
         return true;
     }
 }
