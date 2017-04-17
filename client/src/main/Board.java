a6d9c846aebc657b8a43e6c1160def01464e330a
刘俊延 <13925588730@163.com>
2017-04-17 19:56:42 +0800

    Add board and stone functions.

--- client/src/main/Board.java
+++ client/src/main/Board.java
@@ -1,22 +1,44 @@
 package src.main;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 /**
  * Created by touhoudoge on 2017/4/16.
  */
 public class Board {
 
-    // -1 is black, 1 is white
-    private int[][] board = new int[19][19];
+    private Stone[][] board = new Stone[19][19];
 
     public Board(){
-        for(int i = 0;i<19;++i){
-            for(int j = 0;j<19;++j){
-                board[i][j] = 0;
+        for(int i = 0; i < 19; ++i){
+            for(int j = 0; j < 19; ++j){
+                board[i][j] = new Stone();
             }
         }
     }
 
     public void chessMoves(int x,int y,int color){
 
+    }
+
+    public void draw(Circle circle, int posX, int posY, int color){
+        if(color == Stone.Black){
+            circle.setFill(Color.BLACK);
+        }
+        else{
+            circle.setFill(Color.WHITE);
+        }
+        circle.setLayoutX(posX);
+        circle.setLayoutY(posY);
+        circle.setRadius(10);
+    }
+
+    public boolean check(int x, int y, int color){
+        if(board[x][y].getType() != Stone.None){
+            return false;
+        }
+        board[x][y].setType(color);
+        return true;
     }
 }
--- client/src/main/view/ChessBox.java
+++ client/src/main/view/ChessBox.java
@@ -16,75 +16,103 @@ import java.util.ResourceBundle;
 /**
  * Created by touhoudoge on 2017/4/7.
  */
-public class ChessBox implements Initializable{
+public class ChessBox implements Initializable {
 
     private Timer timer;
     private Board board = new Board();
+    private int turns = -1;
+    @FXML
+    private Pane chessPane;
 
-    @FXML private Pane chessPane;
+    private static final int borderGap = 20;
+    private static final int stoneGap = 30;
+    private static final int xLen = 18 * stoneGap + 2 * borderGap;
+    private static final int yLen = 18 * stoneGap + 2 * borderGap;
+    private int posX;
+    private int posY;
 
     @FXML
-    private void onClick(MouseEvent event){
+    private void onClick(MouseEvent event) {
         /************* test ********************/
+        posX = (int) event.getX();
+        posY = (int) event.getY();
+        if (isClickVaild()) {
+            System.out.println("px=" + posX + ", py=" + posY);
+            System.out.println("iX=" + (posX - borderGap) / stoneGap + ", iY=" + (posY - borderGap) / stoneGap);
+            board.chessMoves((posX - borderGap) / stoneGap, (posY - borderGap) / stoneGap, turns);
             Circle circle = new Circle();
-            circle.setFill(Color.BLACK);
-            int x = (int)event.getX();
-            int y = (int)event.getY();
-            if(x<10 || x>550 || y<10 || y>550)
-                return;
-            if((x-10)%30<7 && (y-10)%30<7){
-                x = (x-10)/30*30+10;
-                y = (y-10)/30*30+10;
-            }else if((x-10)%30<7 && (y-10)%30>23){
-                x = (x-10)/30*30+10;
-                y = ((y-10)/30+1)*30+10;
-            }else if((x-10)%30>23 && (y-10)%30<7){
-                x = ((x-10)/30+1)*30+10;
-                y = (y-10)/30*30+10;
-            }else if((x-10)%30>23 && (y-10)%30>23){
-                x = ((x-10)/30+1)*30+10;
-                y = ((y-10)/30+1)*30+10;
-            }else{
-                return;
-            }
-            board.chessMoves((x-10)/30,(y-10)/30,1);
-            circle.setLayoutX(x);
-            circle.setLayoutY(y);
-            circle.setRadius(10);
+            board.draw(circle, posX, posY, turns);
             chessPane.getChildren().add(circle);
             timer.start();
+            turns = -turns;
+        }
         /************* test ********************/
+    }
+
+    private boolean isClickVaild() {
+        if (posX < borderGap || posX > xLen - borderGap
+                || posY < borderGap || posY > yLen - borderGap) {
+            return false;
+        }
+        if ((posX - borderGap) % stoneGap < 8 && (posY - borderGap) % stoneGap < 8) {
+            posX = (posX - borderGap) / stoneGap * stoneGap + borderGap;
+            posY = (posY - borderGap) / stoneGap * stoneGap + borderGap;
+        } else if ((posX - borderGap) % stoneGap < 8 && (posY - borderGap) % stoneGap > 22) {
+            posX = (posX - borderGap) / stoneGap * stoneGap + borderGap;
+            posY = ((posY - borderGap) / stoneGap + 1) * stoneGap + borderGap;
+        } else if ((posX - borderGap) % stoneGap > 22 && (posY - borderGap) % stoneGap < 8) {
+            posX = ((posX - borderGap) / stoneGap + 1) * stoneGap + borderGap;
+            posY = (posY - borderGap) / stoneGap * stoneGap + borderGap;
+        } else if ((posX - borderGap) % stoneGap > 22 && (posY - borderGap) % stoneGap > 22) {
+            posX = ((posX - borderGap) / stoneGap + 1) * stoneGap + borderGap;
+            posY = ((posY - borderGap) / stoneGap + 1) * stoneGap + borderGap;
+        } else {
+            return false;
+        }
+        if (!board.check((posX - borderGap) / stoneGap, (posY - borderGap) / stoneGap, turns)) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Rectangle rec = new Rectangle(0,0,560,560);
-        rec.setFill(Color.rgb(249,214,91));
-        chessPane.getChildren().add(rec);
+        drawBoard();
+        drawLine();
+        drawStar();
+    }
 
+    private void drawBoard() {
+        Rectangle rec = new Rectangle(0, 0, 560, 560);
+        rec.setFill(Color.rgb(249, 214, 91));
+        chessPane.getChildren().add(rec);
+    }
+
+    private void drawLine() {
         Line line;
-        line = new Line(0,570,0,570);
-        chessPane.getChildren().add(line);
-        for(int i = 0;i<19;++i){
-            line = new Line(i*30+10,10,i*30+10,550);
+        for (int i = 0; i < 19; ++i) {
+            line = new Line(borderGap, i * stoneGap + borderGap, xLen - borderGap, i * stoneGap + borderGap);
+            chessPane.getChildren().add(line);
+        }
+        for (int i = 0; i < 19; ++i) {
+            line = new Line(i * stoneGap + borderGap, borderGap, i * stoneGap + borderGap, yLen - borderGap);
             line.setStroke(Color.BLACK);
             chessPane.getChildren().add(line);
         }
-        for(int i = 0;i<19;++i){
-            line = new Line(10,i*30+10,550,i*30+10);
-            chessPane.getChildren().add(line);
-        }
+    }
+
+    private void drawStar() {
         int x = 3;
         int y;
         Circle circle;
-        for(int i = 0;i<3;++i){
+        for (int i = 0; i < 3; ++i) {
             y = 3;
-            for(int j = 0;j<3;++j){
+            for (int j = 0; j < 3; ++j) {
                 circle = new Circle();
                 circle.setFill(Color.BLACK);
                 circle.setRadius(3);
-                circle.setLayoutX(x*30+10);
-                circle.setLayoutY(y*30+10);
+                circle.setLayoutX(x * stoneGap + borderGap);
+                circle.setLayoutY(y * stoneGap + borderGap);
                 chessPane.getChildren().add(circle);
                 y = y + 6;
             }
@@ -92,7 +120,7 @@ public class ChessBox implements Initializable{
         }
     }
 
-    public void setTimer(Timer timer){
+    public void setTimer(Timer timer) {
         this.timer = timer;
     }
 }
