cdab963a848702dc69507a2be0e899f848f67836
刘俊延 <13925588730@163.com>
2017-04-19 22:26:06 +0800

    Fix Logic bug.

--- client/src/main/Core.java
+++ client/src/main/Core.java
@@ -5,62 +5,43 @@ package src.main;
  */
 public class Core {
 
-    public int action(int x, int y, int color){
+    private static boolean hasLiberty;
+    private static boolean hasKilled;
+
+    public static int action(int x, int y, int color) {
         Stone up = Board.stones[x][y].up();
         Stone down = Board.stones[x][y].down();
         Stone left = Board.stones[x][y].left();
         Stone right = Board.stones[x][y].right();
-        boolean hasLiberty = false;
-        boolean hasKilled = false;
-        if(up != null){
-            if(up.type == Stone.None || (up.type == color && liberty(up) > 1)){
-                hasLiberty = true;
-            }
-            else if (up.type == -color && liberty(up) == 1){
-                hasKilled = true;
-                Board.killed.add(Board.chainMap.get(up));
-            }
+        hasLiberty = false;
+        hasKilled = false;
+        if (up != null) {
+            actionWith(up, color);
         }
-        if(down != null){
-            if(down.type == Stone.None || (down.type == color && liberty(down) > 1)){
-                hasLiberty = true;
-            }
-            else if (down.type == -color && liberty(down) == 1){
-                hasKilled = true;
-                Board.killed.add(Board.chainMap.get(down));
-            }
+        if (down != null) {
+            actionWith(down, color);
         }
-        if(left != null){
-            if(left.type == Stone.None || (left.type == color && liberty(left) > 1)){
-                hasLiberty = true;
-            }
-            else if (left.type == -color && liberty(left) == 1){
-                hasKilled = true;
-                Board.killed.add(Board.chainMap.get(left));
-            }
+        if (left != null) {
+            actionWith(left, color);
         }
-        if(right != null){
-            if(right.type == Stone.None || (right.type == color && liberty(right) > 1)){
-                hasLiberty = true;
-            }
-            else if (right.type == -color && liberty(right) == 1){
-                hasKilled = true;
-                Board.killed.add(Board.chainMap.get(right));
-            }
+        if (right != null) {
+            actionWith(right, color);
         }
-        if(hasKilled){
-            return Action.KILL;
-        }
-        if(hasLiberty){
-            return Action.PLACE;
-        }
-        return Action.INVALID;
+        return hasKilled ? Action.KILL : hasLiberty ? Action.PLACE : Action.INVALID;
     }
 
+    private static void actionWith(Stone stone, int color) {
+        if (stone.type == Stone.None || (stone.type == color && liberty(stone) > 1)) {
+            hasLiberty = true;
+        } else if (stone.type == -color && liberty(stone) == 1) {
+            hasKilled = true;
+            Board.killed.add(Board.chainMap.get(stone));
+        }
+    }
 
-    public int liberty(Stone stone){
+    public static int liberty(Stone stone) {
         int chain = Board.chainMap.get(stone);
-        System.out.println("Point ("+stone.x+","+stone.y+") in chain "+chain+" has liberty "+Board.liberty.get(chain).size());
+        System.out.println("Point (" + stone.x + "," + stone.y + ") in chain " + chain + " has liberty " + Board.liberty.get(chain).size());
         return Board.liberty.get(Board.chainMap.get(stone)).size();
     }
 
