3a8bad868e5cd59c589150444be99d7d60233a76
刘俊延 <13925588730@163.com>
2017-04-25 11:27:59 +0800

    Fix.

--- client/src/main/Client.java
+++ client/src/main/Client.java
@@ -24,8 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-public class Client extends Application
-{
+public class Client extends Application {
     private Stage primaryStage;
     private Stage createRoomStage;
     private Stage gameStage;
@@ -33,37 +32,39 @@ public class Client extends Application
     private Stage signupStage = null;
     private UserInfo account;
     private Connect connect;
-    private ArrayList playerList=new ArrayList(); 
+    private ArrayList playerList = new ArrayList();
+
     public Client() {
-    	connect = new Connect();
-    	lobbyStage = new Stage();
-    	signupStage = new Stage();
-    	
+//        connect = new Connect();
+        lobbyStage = new Stage();
+        signupStage = new Stage();
     }
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         this.primaryStage = primaryStage;
         primaryStage.setTitle("MicroOnlineGo");
         gotoLogin();
     }
 
-    public void gotoLogin() throws Exception{
-
-        LoginController loginController = (LoginController) changeStage("view/Login.fxml",primaryStage);
+    public void gotoLogin() throws Exception {
+        LoginController loginController = (LoginController) changeStage("view/Login.fxml", primaryStage);
         loginController.setClient(this);
+        loginController.resetAccount();
+        loginController.resetPassword();
     }
 
-    public void gotoLobby() throws Exception{
-        LobbyController lobbyController = (LobbyController) changeStage("view/Lobby.fxml",lobbyStage);
+    public void gotoLobby() throws Exception {
+        LobbyController lobbyController = (LobbyController) changeStage("view/Lobby.fxml", lobbyStage);
         lobbyController.setClient(this);
     }
 
-    public void gotoSignUp() throws Exception{
-        SignUpController signUpController = (SignUpController) changeStage("view/SignUp.fxml",signupStage);
+    public void gotoSignUp() throws Exception {
+        SignUpController signUpController = (SignUpController) changeStage("view/SignUp.fxml", signupStage);
         signUpController.setClient(this);
     }
 
-    public void backToLobby(){
+    public void backToLobby() {
         createRoomStage.close();
     }
 
@@ -81,7 +82,8 @@ public class Client extends Application
         createRoomController.setClient(this);
         createRoomController.setRoomList(roomList);
     }
-    public void gotoGame() throws Exception{
+
+    public void gotoGame() throws Exception {
         gameStage = new Stage();
         FXMLLoader loader = new FXMLLoader();
         loader.setBuilderFactory(new JavaFXBuilderFactory());
@@ -104,69 +106,71 @@ public class Client extends Application
         primaryStage.show();
         return (Initializable) loader.getController();
     }
-    
-    private Initializable changeStage(String fxml,Stage stage) {
-    	//create a new stage
-    	/*if(stage == null)
+
+    private Initializable changeStage(String fxml, Stage stage) {
+        //create a new stage
+        /*if(stage == null)
     		stage = new Stage();*/
-    	//create the fxml loader
-    	FXMLLoader loader = new FXMLLoader();
-    	//set the location of the fxml
-    	loader.setLocation(getClass().getResource(fxml));
-    	//get the pane
-    	try {
-			Pane pane = loader.load();
-			//create a new scene with the pane
-			Scene scene = new Scene(pane);
-			//set the scene
-			stage.setScene(scene);
-			//show the stage
-			stage.show();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			return (Initializable)loader.getController();
-		}
+        //create the fxml loader
+        FXMLLoader loader = new FXMLLoader();
+        //set the location of the fxml
+        loader.setLocation(getClass().getResource(fxml));
+        //get the pane
+        try {
+            Pane pane = loader.load();
+            //create a new scene with the pane
+            Scene scene = new Scene(pane);
+            //set the scene
+            stage.setScene(scene);
+            //show the stage
+            stage.show();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            return (Initializable) loader.getController();
+        }
     }
     //get the connection
-    
+
     public Connect getConnect() {
-		return connect;
-	}
-    public void setAccount(UserInfo account){
+        return connect;
+    }
+
+    public void setAccount(UserInfo account) {
         this.account = account;
     }
 
-    
-	public UserInfo getAccount(){
+    public UserInfo getAccount() {
         return account;
     }
-	
-	
+
     public Stage getPrimaryStage() {
-		return primaryStage;
-	}
-	public Stage getCreateRoomStage() {
-		return createRoomStage;
-	}
-	public Stage getGameStage() {
-		return gameStage;
-	}
-	public Stage getLobbyStage() {
-		return lobbyStage;
-	}
-	public Stage getsignupStage() {
-		return signupStage;
-	}
-	
-	public ArrayList getPlayerList() {
-		return playerList;
-	}
-	public static void main(String[] args)
-    {
-        launch(args);
+        return primaryStage;
     }
 
+    public Stage getCreateRoomStage() {
+        return createRoomStage;
+    }
+
+    public Stage getGameStage() {
+        return gameStage;
+    }
+
+    public Stage getLobbyStage() {
+        return lobbyStage;
+    }
+
+    public Stage getsignupStage() {
+        return signupStage;
+    }
+
+    public ArrayList getPlayerList() {
+        return playerList;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 }
