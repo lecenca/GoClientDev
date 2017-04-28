ec13aca56c7270e6919daaa6b71a03c398c33c0a
lecenca <isbnmiku@gmail.com>
2017-04-29 00:32:26 +0800

    update CreateRoom

--- client/src/main/view/CreateRoomController.java
+++ client/src/main/view/CreateRoomController.java
@@ -1,8 +1,10 @@
 package src.main.view;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import src.main.Client;
@@ -25,6 +27,10 @@ public class CreateRoomController implements Initializable {
     @FXML private TextField passwordField;
     @FXML private Button createRoomBtn;
     @FXML private Button backBtn;
+    @FXML private ComboBox mainTime;
+    @FXML private ComboBox poriod;
+    @FXML private ComboBox periodTimes;
+    @FXML private ComboBox komi;
 
     public void setClient(Client client){
         this.client = client;
@@ -56,6 +62,39 @@ public class CreateRoomController implements Initializable {
         /************* test ********************/
     }
 
+    private void initComboBox(){
+        mainTime.setItems(FXCollections.observableArrayList());
+        poriod.setItems(FXCollections.observableArrayList());
+        periodTimes.setItems(FXCollections.observableArrayList());
+        komi.setItems(FXCollections.observableArrayList());
+        mainTime.getItems().add("1分");
+        for (int i = 5; i <= 20; i = i +5) {
+            mainTime.getItems().add(String.valueOf(i)+"分");
+        }
+        mainTime.getItems().add("30分");
+        mainTime.getItems().add("40分");
+        mainTime.getItems().add("60分");
+        mainTime.getItems().add("90分");
+        poriod.getItems().add("15秒");
+        for (int i = 20; i <= 60; i = i + 10){
+            poriod.getItems().add(String.valueOf(i)+"秒");
+        }
+        for (int i = 1; i <= 7; i = i + 2){
+           periodTimes.getItems().add(String.valueOf(i)+"次");
+        }
+        komi.getItems().add("让先");
+        komi.getItems().add("黑贴3.5目");
+        komi.getItems().add("黑贴6.5目");
+        periodTimes.getItems().add("10次");
+        mainTime.setVisibleRowCount(6);
+        mainTime.getSelectionModel().select(5);
+        poriod.setVisibleRowCount(6);
+        poriod.getSelectionModel().select(2);
+        periodTimes.setVisibleRowCount(6);
+        periodTimes.getSelectionModel().select(1);
+        komi.getSelectionModel().select(1);
+    }
+
     @FXML
     private void back(){
         client.backToLobby();
@@ -64,5 +103,7 @@ public class CreateRoomController implements Initializable {
     public void setRoomList(TableView<RoomListCell> roomList) {this.roomList = roomList;}
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {}
+    public void initialize(URL location, ResourceBundle resources) {
+        initComboBox();
+    }
 }
